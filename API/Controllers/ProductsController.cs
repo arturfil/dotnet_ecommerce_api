71573f8c796f5f545baf78f661e441c1add68056
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2020-04-15 18:31:44 -0400

    added Error Validation and Swagger documentation along with some helper classes to declutter the startup file

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -7,12 +7,12 @@ using Core.Specifications;
 using API.Dtos;
 using System.Linq;
 using AutoMapper;
+using API.Errors;
+using Microsoft.AspNetCore.Http;
 
 namespace API.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  public class ProductsController : ControllerBase
+  public class ProductsController : BaseApiController
   {
     private readonly IGenericRepository<Product> _productsRepo;
     private readonly IGenericRepository<ProductBrand> _productBrandRepo;
@@ -30,6 +30,7 @@ namespace API.Controllers
       _mapper = mapper;
     }
 
+    // Get/Products
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
     {
@@ -38,20 +39,28 @@ namespace API.Controllers
       return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
     }
 
+    // Get/Products/:id
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     {
       var spec = new ProductsWithTypesAndBrandsSpecification(id);
       var product = await _productsRepo.GetEntitiyWithSpec(spec);
+      
+      if (product == null) return NotFound(new ApiResponse(404));
+      
       return _mapper.Map<Product, ProductToReturnDto>(product);
     }
 
+    // Get/ProductsBrands
     [HttpGet("brands")]
     public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
     {
       return Ok(await _productBrandRepo.ListAsync());
     }
 
+    // Get/ProductsTypes
     [HttpGet("types")]
     public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductType()
     {
---
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace API.Extensions
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services) {
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Ecommerce API", Version = "v1"});
+            });
+            return services;
+        }
+    }
+}
