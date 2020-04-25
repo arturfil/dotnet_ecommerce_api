f7eaa533942d9de9f8da1f5b901f520939365256
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2020-04-25 05:50:43 -0400

    added an http request to the get//products endpoint

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Mechcables';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=2&pageIndex=0').subscribe((response: any) =>  {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
 }
