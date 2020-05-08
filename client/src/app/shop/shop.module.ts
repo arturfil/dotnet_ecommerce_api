308008a8ded35850e25e1db433cadde3341af4c5
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2020-05-08 15:41:00 -0400

    added pagination and filtering to client side, right now the count for the request is not functioning as expected, still needs refactoring in ProductWithFiltersForCountSpec...

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
-
-
+import { SharedModule } from '../shared/shared.module';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @NgModule({
   declarations: [ShopComponent, ProductItemComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
   ],
   exports: [ShopComponent]
 })
