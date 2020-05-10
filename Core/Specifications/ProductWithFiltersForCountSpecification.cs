82e8cfc2a44bb9805d420add3666faf28e513a6d
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2020-05-09 20:27:47 -0400

    corrected the ProductWithFiltersForCountSpecification.cs component, count is now being sent as intended and with the code intended

--- Core/Specifications/ProductWithFiltersForCountSpecification.cs
+++ Core/Specifications/ProductWithFiltersForCountSpecification.cs
@@ -6,18 +6,10 @@ namespace Core.Specifications
   {
     public ProductWithFiltersForCountSpecification(ProductSpecParams productParams)
             : base(x =>
-              (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId)
-              && (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+               (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search))
+               && (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId)
+               && (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
             )
     { }
   }
 }
-
-
-// public ProductWithFiltersForCountSpecification(ProductSpecParams productParams)
-//             : base(x =>
-//                (!string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search))
-//                && (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId)
-//                && (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
-//             )
-// { }
