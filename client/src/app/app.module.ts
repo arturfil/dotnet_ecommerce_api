a82041533a3619584f434206682feec35bdc5271
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2020-05-11 20:56:53 -0400

    added single product component, also added links and components to the routing, added sub-routing for lazy-loading

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {CoreModule} from './core/core.module';
 import { ShopModule } from './shop/shop.module';
 import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -17,9 +18,9 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HomeModule,
     HttpClientModule,
     CoreModule,
-    ShopModule,
     SharedModule
   ],
   providers: [],
