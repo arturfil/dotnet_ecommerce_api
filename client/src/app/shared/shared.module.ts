4408b1393a3d1bb0ad0c112b857197dc97eaee97
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2020-05-31 03:57:25 -0400

    added login, register and client validations

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -7,12 +7,14 @@ import { PagingHeaderComponent } from './components/paging-header/paging-header.
 import { PagerComponent } from './components/pager/pager.component';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TextInputComponent } from './components/text-input/text-input.component';
 
 @NgModule({
   declarations: [
     PagingHeaderComponent,
     PagerComponent,
-    OrderTotalsComponent
+    OrderTotalsComponent,
+    TextInputComponent
   ],
   imports: [
     CommonModule,
@@ -27,6 +29,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     PagerComponent,
     CarouselModule,
     OrderTotalsComponent,
+    TextInputComponent,
     ReactiveFormsModule,
     BsDropdownModule
   ]
