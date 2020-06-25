cb029087085525953106abb090867f150ec967b0
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2020-06-25 00:20:57 -0400

    missing to refactor the order totals with context of basket component, the amounts in the summary arent appearing

--- client/src/app/shared/components/order-totals/order-totals.component.ts
+++ client/src/app/shared/components/order-totals/order-totals.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IBasketTotals } from '../../models/basket';
+import { BasketService } from 'src/app/basket/basket.service';
 
 @Component({
   selector: 'app-order-totals',
@@ -6,8 +9,8 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./order-totals.component.scss']
 })
 export class OrderTotalsComponent implements OnInit {
-  @Input() subtotal: number;
   @Input() shippingPrice: number;
+  @Input() subtotal: number;
   @Input() total: number;
 
   constructor() { }
