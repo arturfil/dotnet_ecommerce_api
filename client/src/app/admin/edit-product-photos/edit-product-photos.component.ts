7aa6b0d7cf60329471cff02d487177845661b6a5
arturfil <arturfil@hotmail.com>
2020-07-18 15:19:02 -0400

    refactored a variable that was missing to be refactored

---
+++ client/src/app/admin/edit-product-photos/edit-product-photos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-product-photos',
+  templateUrl: './edit-product-photos.component.html',
+  styleUrls: ['./edit-product-photos.component.scss']
+})
+export class EditProductPhotosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
